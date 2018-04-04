749d709b304008165ec94a74a7e332df4a65dcef
Kai Curry <kai@cape.io>
2018-04-04 14:27:56 -0400

    init code

--- README.md
+++ README.md
@@ -1,6 +1,46 @@
-`subBits(num, start, length)`
-`getUint32(Uint8Array, start, length)`
-`getDataView(Uint8Array, start, length)`
-`getBytes(Uint8Array, start, length)`
-`get6Array()` - Handy for Base64 encoding.
-`getIntOrBytes(Uint8Array, start, length, get6 = false)`
+# get-bits v1.0.0
+
+Use this when you need to get/slice some bits at an arbitrary bit index from a byte or Uint8Array. Something like a bit-array, bit vector, bit string, bit set, or whatever they are called. Should work equally well in browser or in node. These are only getters.
+
+## Usage
+### Install
+
+If you're using Node.js type:
+
+```shell
+npm install --save bits
+```
+
+or
+
+```shell
+yarn add bits
+```
+
+### Require
+
+```javascript
+const { getBytes } = require('get-bits')
+```
+
+or
+
+```javascript
+import { getBytes } from 'get-bits'
+```
+
+## API
+
+### `subByte(num, start, length)`
+Bits from a number. Assuming you're working with an 8 bit number.
+
+### `getUint32(Uint8Array, start, length)`
+
+### `getDataView(Uint8Array, start, length)`
+
+### `getBytes(Uint8Array, start, length)`
+
+### `get6Array()`
+Handy for Base64 encoding.
+
+### `getIntOrBytes(Uint8Array, start, length, [get6=false])`
---
+++ src/index.test.js
@@ -0,0 +1,40 @@
+/* globals describe test expect */
+import { get6Array, getDataView, getUint32, getBytes, subByte } from './index'
+import { ais1byteArray, msg5byteArray } from './mock'
+
+describe('subByte', () => {
+  test('returns number', () => {
+    const num1 = 0b11111111 // 255
+    expect(subByte(num1, 0, 3)).toBe(0b111)
+    expect(subByte(num1, 1, 7)).toBe(0b1111111)
+    const num2 = 0b10110101 // 181
+    expect(subByte(num2, 2, 3)).toBe(0b110)
+    expect(subByte(num2, 3, 5)).toBe(0b10101)
+    const num3 = 0b10100101 // 165
+    expect(subByte(num3, 3, 3)).toBe(0b1)
+    const num4 = 0b10000001 // 129
+    expect(subByte(num4, 3, 3)).toBe(0b0)
+    const num5 = 0b00011100
+    expect(subByte(num5, 3, 4)).toBe(0b1110)
+  })
+})
+describe('getUint32', () => {
+  test('returns bits', () => {
+    expect(getUint32(ais1byteArray, 0, 6)).toBe(18)
+    expect(getUint32(ais1byteArray, 6, 2)).toBe(0)
+    expect(getUint32(ais1byteArray, 8, 30)).toBe(367572690)
+    expect(getUint32(msg5byteArray, 8, 30)).toBe(378112714)
+    const data = getBytes(msg5byteArray, 0, 48)
+    expect(data).toEqual(msg5byteArray.slice(0, 6))
+    expect(getUint32(data, 8, 30)).toBe(378112714)
+    const data2 = get6Array(msg5byteArray, 112, 120)
+    expect(data2).toEqual(new Uint8Array([
+      2, 12, 1, 3, 11, 32, 7, 15, 12, 4, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32]))
+
+    // const data3 = get6Array(msg5byteArray, 70, 42)
+    // console.log(data3)
+  })
+})
+describe('getDataView', () => {
+  expect(getDataView(msg5byteArray, 0, 6).getUint8(0)).toEqual(5)
+})
